6c8e368b7c9b84f74da65fe019c34bbd3a71b708
PiyushAhinave <piyushahinave@gmail.com>
2023-10-17 07:30:54 +0530

    javascript concepts start

---
+++ concept.js
@@ -0,0 +1,38 @@
+//words and keywords
+//words are words which dont have meaning in Javascript
+//keywprds are words which have meaning in javascript such as for if else etc.
+
+
+//var and const and let
+//var-variable is container in which we can store value or data which can be changed
+//const-constant is used to store value which can not be changed
+
+/*var a = "dulha"
+var b = "dulhan"
+ console.log(a+" weds "+b)
+ a = "dushman" 
+console.log(a+" weds "+b)*/
+//in above example value of a will change to dushman
+
+/*const a = "dulha"
+const b = "dulhan"
+console.log(a+ "weds" +b)
+a = "dushman"
+console.log(a+ "weds" +b)*/
+//in above example value will not change it will throw error
+
+//Hoisting - variables and functions are hoisted which means there declaration is moved on the top of code
+/*var a ;
+console.log(a);
+a = "piyush"*/
+/*it will show undefined which means variable is declared but not given value*/
+/*not-defined means variable is not declared or does not exist*/
+
+//types in js
+//primitive and reference
+/*Reference - any value in which on copying it that passes its reference of main value is reference value
+reference values shown in {},[],() 
+it can contain multiple values*/
+
+/*Primitives - this are values in which we get real values after copying 
+primitives are number,string,boolean etc*/
